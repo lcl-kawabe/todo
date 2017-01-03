0db6998d0c3f4d9ee6f640f5bf64aa848b5d9e1e
川辺雄介 <kawabe@lclco.com>
2017-01-04 08:11:24 +0900

    初期化

---
+++ src/actions/todos.js
@@ -0,0 +1,77 @@
+import { firebaseDb } from '../firebase/';
+const ref = firebaseDb.ref('todos');
+
+// Subscribe
+function loadTodos() {
+  return dispatch => {
+    ref.off()
+    // valueを購読する。todosに変更があれば、以下の処理が実行される。
+    ref.on('value',
+      (snapshot) => {dispatch(loadTodosSuccess(snapshot))},
+      (error) => {dispatch(loadTodosError(error))}
+    )
+  }
+}
+
+function loadTodosSuccess(snapshot){
+  return {
+    type: 'TODOS_RECEIVE_DATA',
+    data: snapshot.val()
+  }
+}
+
+function loadTodosError(error){
+  return {
+    type: 'TODOS_RECIVE_ERROR',
+    message: error.message
+  }
+}
+
+// CREATE_TASK
+function addTodo(text){
+  return dispatch => {
+    ref.push({
+      text: text,
+      completed: false,
+    })
+      .catch(error => dispatch({
+        type: 'ADD_TASK_ERROR',
+        message: error.message,
+      }));
+  }
+}
+
+// UPDATE_TASK
+function updateTodo(key){
+  return (dispatch, getState) => {
+    let state = getState()
+    let todo = state.todos.filter(todo => todo.key === key)
+
+    // パスのオブジェクトをアップデートします。
+    // updateにはオブジェクトを渡すと差分を自動で更新してくれます。
+    firebaseDb.ref(`todos/${key}`).update({completed: !todo[0].completed})
+      .catch(error => dispatch({
+        type: 'UPDATE_TASK_ERROR',
+        message: error.message,
+      }));
+  }
+}
+
+// DELETE_TASK
+function deleteTodo(key){
+  return dispatch => {
+    // パスのオブジェクトを削除します。
+    firebaseDb.ref(`todos/${key}`).remove()
+      .catch(error => dispatch({
+        type: 'DELETE_TASK_ERROR',
+        message: error.message,
+      }));
+  }
+}
+
+module.exports = {
+  loadTodos,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+}
---
+++ src/firebase/config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDXfrToRc1I2_ZTrtRlDMT3YSycZ-3mt7M",
+    authDomain: "todo-70bb4.firebaseapp.com",
+    databaseURL: "https://todo-70bb4.firebaseio.com",
+    storageBucket: "todo-70bb4.appspot.com",
+    messagingSenderId: "681301284"
+};
